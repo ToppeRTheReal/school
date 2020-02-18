1b136cce39924e58f7fc6e7f41a1c7b7fdb39a6a
ToppeRTheReal <topperhdoriginal@gmail.com>
2020-02-18 11:23:08 +0100

    added stack

---
+++ Semester 2/Stack/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Check> ");
+            string check = Console.ReadLine();
+            Boolean addedOnce = false;
+
+            Stack<char> s = new Stack<char>();
+
+            foreach(char c in check) {
+                if( c == '('){
+                    s.Push(')');
+                    addedOnce = true;
+                }
+                else if( c == '[' ){
+                    s.Push(']');
+                    addedOnce = true;
+                }
+                else if( c == '{' ){
+                    s.Push('}');
+                    addedOnce = true;
+                }
+                else {
+                    if(c == ')' || c == ']' || c == '}') {
+                        if(!addedOnce){
+                            Console.WriteLine("Error");
+                            return;
+                        }
+                        try {
+                            if( c != s.Pop()) {
+                                Console.WriteLine("Error");
+                                return;
+                            }
+                        }catch (InvalidOperationException) {
+                            Console.WriteLine("Error");
+                            return;
+                        }
+                    }
+                }
+            }
+            if(s.Count > 0){
+                Console.WriteLine("Error");
+                return;
+            }
+            Console.WriteLine("Det er korrekt!");
+        }
+    }
+}
+ 
