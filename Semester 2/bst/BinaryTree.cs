12be8a9dd4ebee892ad0ff6e331139ed5ad7e9ca
ToppeRTheReal <topperhdoriginal@gmail.com>
2020-02-25 12:35:46 +0100

    bst

---
+++ Semester 2/bst/BinaryTree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeLibrary
+{
+    class TreeNode
+    {
+        public TreeNode LeftNode { get; set; }
+        public int Data { get; set; }
+        public TreeNode RightNode { get; set; }
+        public TreeNode(int nodeData)
+        {
+            Data = nodeData;
+            LeftNode = RightNode = null;
+        }
+
+        public void Insert(int insertValue)
+        {
+            if (insertValue < Data)
+            {
+                if (LeftNode == null)
+                    LeftNode = new TreeNode(insertValue);
+                else
+                    LeftNode.Insert(insertValue);
+            }
+            else if (insertValue > Data)
+            {
+                if (RightNode == null)
+                    RightNode = new TreeNode(insertValue);
+                else
+                    RightNode.Insert(insertValue);
+            }
+        }
+
+        public class Tree
+        {
+            private TreeNode root;
+            public Tree()
+            {
+                root = null;
+            }
+
+            public void InsertNode(int insertValue)
+            {
+                if (root == null)
+                    root = new TreeNode(insertValue);
+                else
+                    root.Insert(insertValue);
+            }
+
+            public void PreorderTraversal()
+            {
+                PreorderHelper(root);
+            }
+
+            private void PreorderHelper(TreeNode node)
+            {
+                if (node != null)
+                {
+                    Console.Write(node.Data + " ");
+
+                    PreorderHelper(node.LeftNode);
+
+                    PreorderHelper(node.RightNode);
+                }
+            }
+
+            public void InorderTraversal()
+            {
+                InorderHelper(root);
+            }
+
+            private void InorderHelper(TreeNode node)
+            {
+                if (node != null)
+                {
+                    InorderHelper(node.LeftNode);
+
+                    Console.Write(node.Data + " ");
+
+                    InorderHelper(node.RightNode);
+                }
+            }
+
+            public void PostorderTraversal()
+            {
+                PostorderHelper(root);
+            }
+
+            private void PostorderHelper(TreeNode node)
+            {
+                if (node != null)
+                {
+                    PostorderHelper(node.LeftNode);
+
+                    PostorderHelper(node.RightNode);
+
+                    Console.Write(node.Data + " ");
+                }
+            }
+        }
+
+    }
+}
---
+++ Semester 2/bst/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BinaryTreeLibrary;
+using static BinaryTreeLibrary.TreeNode;
+
+namespace bst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree t = new Tree();
+            int insertVal;
+            Random r = new Random();
+            for (int i = 0; i <= 10; i++) {
+                insertVal = r.Next(100);
+                t.InsertNode(insertVal);
+            }
+
+            Console.WriteLine("Preorder");
+            t.PreorderTraversal();
+            Console.WriteLine("\n");
+            Console.WriteLine("Inorder");
+            t.InorderTraversal();
+            Console.WriteLine("\n");
+            Console.WriteLine("Postorder");
+            t.PostorderTraversal();
+            Console.WriteLine("\n");
+        }
+    }
+}
