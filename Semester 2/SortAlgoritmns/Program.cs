a6f5abe2d1eba8a8c5160f38088b9cd2c25746d9
ToppeRTheReal <topperhdoriginal@gmail.com>
2020-03-17 11:06:14 +0100

    Insertion Sort

---
+++ Semester 2/SortAlgoritmns/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using SortAlgoritmns.InsertSort;
+
+namespace SortAlgoritmns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] a = new int[10];
+            var rand = new Random();
+            for (int i = 0; i < 10; i++) { 
+                a[i] = rand.Next(0, 50); 
+            }
+
+            InsertionSort(a);
+        }
+
+         public static void InsertionSort(int[] values) {
+            
+            // loop over data.Length - 1 elements                      
+            for (var next = 1; next < values.Length; ++next) {
+                
+                // store value in current element                       
+                var insert = values[next];
+
+                // initialize location to place element                 
+                var moveItem = next;
+
+                // search for place to put current element              
+                while (moveItem > 0 && values[moveItem - 1] > insert)
+                {
+                    // shift element right one slot                      
+                    values[moveItem] = values[moveItem - 1];
+                    moveItem--;
+                }
+
+            values[moveItem] = insert; // place inserted element    
+            PrintPass(values, next, moveItem); // output pass of algorithm
+      }
+   }
+
+   // display a pass of the algorithm
+        public static void PrintPass(int[] values, int pass, int index) {
+            Console.Write($"after pass {pass}: ");
+
+            // output elements till swapped item
+            for (var i = 0; i < index; ++i)
+            {
+                Console.Write($"{values[i]}  ");
+            }
+
+            Console.Write($"{values[index]}* "); // indicate swap
+
+            // finish outputting array
+            for (var i = index + 1; i < values.Length; ++i)
+            {
+                Console.Write($"{values[i]}  ");
+            }
+
+            Console.Write("\n              "); // for alignment
+
+            // indicate amount of array that is sorted
+            for (var i = 0; i <= pass; ++i)
+            {
+                Console.Write("--  ");
+            }
+
+            Console.WriteLine("\n"); // skip a line in output
+        }
+    }
+}
