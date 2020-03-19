20fb62e4754b5b4950c5afe4926a75f2986af9ae
ToppeRTheReal <topperhdoriginal@gmail.com>
2020-03-19 10:38:32 +0100

    merge sort added

--- Semester 2/SortAlgoritmns/Program.cs
+++ Semester 2/SortAlgoritmns/Program.cs
@@ -9,16 +9,18 @@ namespace SortAlgoritmns
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] a = new int[30000];
-            int[] b = new int[30000];
-            int[] c = new int[30000];
-            int[] d = new int[30000];
+            int[] a = new int[10000];
+            int[] b = new int[10000];
+            int[] c = new int[10000];
+            int[] d = new int[10000];
+            int[] e = new int[10000];
             var rand = new Random();
             for (int i = 0; i < 10000; i++) { 
                 a[i] = rand.Next(0, 50);
                 b[i] = rand.Next(0, 50); 
                 c[i] = rand.Next(0, 50); 
-                d[i] = rand.Next(0, 50); 
+                d[i] = rand.Next(0, 50);
+                e[i] = rand.Next(0, 50);
             }
 
             var stopwatch = new Stopwatch();
@@ -28,25 +30,30 @@ namespace SortAlgoritmns
             InsertionSortClass.InsertionSort(a);
             stopwatch.Stop();
             
-            Console.WriteLine("Insertion took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'millisekunder'"));
+            Console.WriteLine("Insertion took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'ms'"));
 
             stopwatch.Restart();
             //Removed print method with comment
             SelectionSortClass.SelectionSort(b);
             stopwatch.Stop();
-            Console.WriteLine("Selection took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'millisekunder'"));
+            Console.WriteLine("Selection took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'ms'"));
 
             stopwatch.Restart();
             BubbleSortClass.bubbleSort(c);
             stopwatch.Stop();
             //BubbleSortClass.printArray(c);
 
-            Console.WriteLine("bubblesort took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'millisekunder'"));   
+            Console.WriteLine("bubblesort took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'ms'"));   
         
             stopwatch.Restart();
-            Array.Sort(d);
+            Array.Sort(d); // Quicksort :)
             stopwatch.Stop();
-            Console.WriteLine("C# implementation took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'millisekunder'"));
+            Console.WriteLine("C# implementation took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'ms'"));
+        
+            stopwatch.Restart();
+            MergeSortClass.MergeSort(e);
+            stopwatch.Stop();
+            Console.WriteLine("Merge took: " + stopwatch.Elapsed.TotalMilliseconds.ToString("#,##0.00 'ms'"));
         }
     }
 }
